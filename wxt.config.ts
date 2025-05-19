f9f6843f9e2b36db130cc35792b8b6c2af7f7585
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-05-19 15:50:19 +0530

    removed scripting permission

--- wxt.config.ts
+++ wxt.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     version: "0.1.0",
     description:
       "Resume YouTube videos from the exact timestamp, even with history turned off.",
-    permissions: ["storage", "scripting"],
+    permissions: ["storage"],
     host_permissions: ["https://www.youtube.com/*"],
     action: {
       default_popup: "popup/index.html",
